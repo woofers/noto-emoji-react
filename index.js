400bad642c813b1d45599836a8061568da6db4f9
Jaxson Van Doorn <woofers1tt@gmail.com>
2020-05-01 22:04:48 -0700

    Add test cases

--- index.js
+++ index.js
@@ -26,3 +26,9 @@ const toUnicodeBytes = value => {
 }
 
 const escapeBytes = values => `emoji_u${values.join('_')}.svg`
+
+const val = '\u{1F469}\u{1f3fe}\u{200d}\u{1f91d}\u{200d}\u{1F468}\u{1F3FF}'
+const val2 = '\u{1F9D1}\u{1F3FE}'
+const val3 = '\u{1F9DC}\u{1F3FF}\u{200D}\u{2640}\u{FE0F}'
+
+console.log(escapeBytes(toUnicodeBytes(val)))
