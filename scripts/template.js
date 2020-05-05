170b09c50fc2b2be07eedeec57cff30b801d5f80
Jaxson Van Doorn <woofers1tt@gmail.com>
2020-05-05 11:09:16 -0700

    Create ESM module bundle

--- scripts/template.js
+++ scripts/template.js
@@ -1,16 +1,24 @@
-function defaultTemplate(
-  { template },
-  opts,
-  { imports, interfaces, componentName, props, jsx, exports },
-) {
-  const plugins = ['jsx']
-  const typeScriptTpl = template.smart({ plugins })
-  return typeScriptTpl.ast`
+
+const template = esm => {
+  return ({ template }, opts,
+          { imports, interfaces, componentName, props, jsx, exports }) => {
+    const plugins = ['jsx']
+    const typeScriptTpl = template.smart({ plugins })
+    if (esm) {
+      return typeScriptTpl.ast`
+export var ${componentName} = function (${props}) {
+  return ${jsx}
+}
+${componentName.name}.displayName = '${componentName.name}'
+`
+    }
+    return typeScriptTpl.ast`
 module.exports.${componentName} = function (${props}) {
   return ${jsx}
 }
 module.exports.${componentName.name}.displayName = '${componentName.name}'
 `
+  }
 }
 
-module.exports = defaultTemplate
+module.exports = template
